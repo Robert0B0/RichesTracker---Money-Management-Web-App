d2a896fc47a5b0992b9d5b1ca888ca4ecbbe9e45
Robert0B0 <70260478+Robert0B0@users.noreply.github.com>
2020-12-14 01:07:28 -0800

    Update savings.js

--- static/savings/js/savings.js
+++ static/savings/js/savings.js
@@ -1,11 +1,1 @@
-$(document).ready( function () {
-    $('.table_id').DataTable({
-    
-    dom: 'B<"clear">lfrtip',
-    buttons: {
-        name: 'primary',
-        buttons: [ 'copy', 'csv', 'excel', 'pdf' ]
-    
-        }}
-    );
-    } );
+
